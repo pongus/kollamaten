edacce6bbe0c6e925e949128773fc31de0ce2d25
Pontus Gustafsson <pontus.gustafsson@datscha.com>
2020-10-16 16:11:15 +0200

    Updates

---
+++ precache-manifest.b3e94f5393ff6c433d3039c6f553dfac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b291c2134ee99111c6e267b35d32554",
+    "url": "/kollamaten/index.html"
+  },
+  {
+    "revision": "87795dbabacac6e1f482",
+    "url": "/kollamaten/static/css/main.5d45f7ec.chunk.css"
+  },
+  {
+    "revision": "6b17b6dd5ae3e6d29261",
+    "url": "/kollamaten/static/js/2.15d823c0.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/kollamaten/static/js/2.15d823c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87795dbabacac6e1f482",
+    "url": "/kollamaten/static/js/main.ed4058ac.chunk.js"
+  },
+  {
+    "revision": "0c67dc119e89d00a0f79",
+    "url": "/kollamaten/static/js/runtime-main.7c181f8f.js"
+  },
+  {
+    "revision": "d7d5d4588a9f50c99264bc12e4892a7c",
+    "url": "/kollamaten/static/media/OpenSans-Regular.d7d5d458.ttf"
+  },
+  {
+    "revision": "4d580948b7214ca1a435bc4ffd6b0196",
+    "url": "/kollamaten/static/media/arrow-left.4d580948.svg"
+  },
+  {
+    "revision": "b04d9525e029e3960eb5cc5d0b11536d",
+    "url": "/kollamaten/static/media/close.b04d9525.svg"
+  },
+  {
+    "revision": "befc647945eb48c6b68dd22e2851a55d",
+    "url": "/kollamaten/static/media/list.befc6479.txt"
+  },
+  {
+    "revision": "46234caa60c53418dda6c761c373158e",
+    "url": "/kollamaten/static/media/logo.46234caa.svg"
+  },
+  {
+    "revision": "af1db1d5e349e80aba406745a08b0da4",
+    "url": "/kollamaten/static/media/search.af1db1d5.svg"
+  },
+  {
+    "revision": "f37de2cc07d975f943936625eec86da2",
+    "url": "/kollamaten/static/media/web.f37de2cc.config"
+  }
+]);
